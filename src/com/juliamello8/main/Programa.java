02831ae183e764c585960e5c92e296aa80e30373
Julia Mello <46962677+Juliamello8@users.noreply.github.com>
2020-02-28 20:22:33 -0300

    Update Programa.java

--- src/com/juliamello8/main/Programa.java
+++ src/com/juliamello8/main/Programa.java
@@ -1,9 +1,6 @@
 package com.juliamello8.main;
 
 import com.juliamello8.atividade.Desafio1;
-import com.juliamello8.atividade.Desafio2;
-import com.juliamello8.atividade.Desafio3;
-import com.juliamello8.atividade.Desafio4;
 
 public class Programa {
 	
